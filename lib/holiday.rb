43be289635585d8305203224aae32d11cb3dc35f
Richard Cho <96richardohc@gmail.com>
2020-05-15 05:38:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,9 +66,9 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
   puts season.capitalize.to_s + ":"
   puts holiday.capitalize.to_s + ":"
+  binding.pry
   end
   holiday_hash
-  binding.pry
 end
 
 def all_holidays_with_bbq(holiday_hash)
