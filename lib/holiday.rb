4347c0172c935172bbf761987eb703b5b9bc866a
Richard Cho <96richardohc@gmail.com>
2020-05-15 06:18:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 holiday_hash.each do |season, holiday|
 holiday.each do |a, b|
-  puts b.to_s
+  if b.to_s == "BBQ"
+    return season
     end
   end
 end
